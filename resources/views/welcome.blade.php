64f5a98f9f692535fe9d290a25c8e82fb8e73d25
akbartitowic <akbartitowic@gmail.com>
2026-09-03 10:26:57 +0700

    fix(view): use dynamic app_name in welcome blade title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Noohtify</title>
+    <title>{{ $app_name ?? 'MyActivity' }}</title>
     <link rel="icon" type="image/png" href="{{ $favicon_url ?? '/favicon.png' }}">
     @viteReactRefresh
     @vite(['resources/js/index.css', 'resources/js/main.jsx'])
